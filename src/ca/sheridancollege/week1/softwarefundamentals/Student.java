50c00bf1a2019950429a1c9b5461300aff2ca257
jgame <yetmanj@sheridancollege.ca>
2020-01-15 11:15:22 -0500

    Revert "4th commit"
    This reverts commit d7c44d6cf87f21e05aa731dd93b911743bc1c58d.

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -14,8 +14,7 @@ public class Student {
     private String name;
     private double GPA;
     private String email;
-    private int ID;
-    
+
     public double getGPA() {
         return GPA;
     }
