d7c44d6cf87f21e05aa731dd93b911743bc1c58d
jgame <yetmanj@sheridancollege.ca>
2020-01-15 11:14:52 -0500

    4th commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -14,7 +14,8 @@ public class Student {
     private String name;
     private double GPA;
     private String email;
-
+    private int ID;
+    
     public double getGPA() {
         return GPA;
     }
